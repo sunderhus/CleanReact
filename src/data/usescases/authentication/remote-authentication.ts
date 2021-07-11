5c88f2104394abc748a9af6202b782756c893eef
Sunderhus <matheus.sunderhus@gmail.com>
2021-07-11 17:18:32 -0300

    feat: ensure RemoteAuthentication throws UnexpectedError when not 200 or 401

--- src/data/usescases/authentication/remote-authentication.ts
+++ src/data/usescases/authentication/remote-authentication.ts
@@ -1,5 +1,6 @@
 import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InvalidCredentialsError } from '@/domain/Errors/InvalidCredentialsError'
+import { UnexpectedError } from '@/domain/Errors/UnexpectedError'
 import { AuthenticationParams } from '@/domain/usecases/authentication'
 import { IHttpPostClient } from 'data/protocols/http/http-post-client'
 
@@ -13,8 +14,9 @@ export class RemoteAuthentication {
     const httpResponse = await this.httpPostClient.post({ url: this.url, body: params })
 
     switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok : break
       case HttpStatusCode.unauthorized: throw new InvalidCredentialsError()
-      default: return Promise.resolve()
+      default: throw new UnexpectedError()
     }
   }
 }
