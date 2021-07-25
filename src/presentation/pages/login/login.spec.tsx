6cdb80a3fcf304303a2fd78a623162bbd8e5623f
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-07-25 19:34:14 -0300

    test: ensure login starts with submit button disabled

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,4 +9,11 @@ describe('Login Component', () => {
 
     expect(errorWrap.childElementCount).toBe(0)
   })
+
+  test('Should not start with submite button enabled', () => {
+    const { getByTestId } = render(<Login />)
+    const submitButton = getByTestId('submit') as HTMLButtonElement
+
+    expect(submitButton.disabled).toBe(true)
+  })
 })
