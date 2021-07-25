dd6538f7e475e7376fc15c109f0b19e41ff9b276
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-07-25 20:12:15 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,29 +1,42 @@
 import React from 'react'
-import { render } from '@testing-library/react'
+import { render, RenderResult } from '@testing-library/react'
 import Login from './index'
+
+type SutTypes ={
+  sut: RenderResult
+}
+
+const makeSut = (): SutTypes => {
+  const sut = render(<Login />)
+
+  return {
+    sut
+  }
+}
 
 describe('Login Component', () => {
   test('Should not render Spinner and erro on start', () => {
-    const { getByTestId } = render(<Login />)
-    const errorWrap = getByTestId('error-wrap')
+    const { sut } = makeSut()
+
+    const errorWrap = sut.getByTestId('error-wrap')
 
     expect(errorWrap.childElementCount).toBe(0)
   })
 
   test('Should not start with submite button enabled', () => {
-    const { getByTestId } = render(<Login />)
-    const submitButton = getByTestId('submit') as HTMLButtonElement
+    const { sut } = makeSut()
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
 
     expect(submitButton.disabled).toBe(true)
   })
 
   test('Should start with defaults inputs status titles ', () => {
-    const { getByTestId } = render(<Login />)
-    const emailStatus = getByTestId('email-status') as HTMLButtonElement
+    const { sut } = makeSut()
+    const emailStatus = sut.getByTestId('email-status') as HTMLButtonElement
     expect(emailStatus.title).toBe('Campo obrigatório')
     expect(emailStatus.textContent).toBe('🔴')
 
-    const passwordStatus = getByTestId('password-status') as HTMLButtonElement
+    const passwordStatus = sut.getByTestId('password-status') as HTMLButtonElement
     expect(passwordStatus.title).toBe('Campo obrigatório')
     expect(passwordStatus.textContent).toBe('🔴')
   })
