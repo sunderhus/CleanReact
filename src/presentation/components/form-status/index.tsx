c7a71cabaf83ea19bc4d2e48133b916a87a9f5d7
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-07-25 19:23:49 -0300

    feat: ensure Login will not load spinner and error on start

--- src/presentation/components/form-status/index.tsx
+++ src/presentation/components/form-status/index.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
+
 import Spinner from '../spinner'
 import Styles from './styles.scss'
+import FormContext from '@/presentation/contexts/form'
 
 const FormStatus: React.FC = () => {
+  const { isLoading, errorMessage } = useContext(FormContext)
+
   return (
-    <div className={Styles.errorWrap}>
-      <Spinner className={Styles.spinner}/>
-      <span className={Styles.error}>Erro</span>
+    <div data-testid="error-wrap" className={Styles.errorWrap}>
+      {isLoading && <Spinner className={Styles.spinner}/>}
+      {errorMessage && <span className={Styles.error}>{errorMessage}</span>}
     </div>
   )
 }
--- src/presentation/pages/login/index.tsx
+++ src/presentation/pages/login/index.tsx
@@ -1,19 +1,32 @@
 import { Footer, FormStatus, Input, LoginHeader } from '@/presentation/components'
-import React from 'react'
+import React, { useState } from 'react'
 import Styles from './styles.scss'
+import FormContext from '../../contexts/form'
+
+type StateProps = {
+  isLoading: boolean
+  errorMessage: string
+}
 
 const Login: React.FC = () => {
+  const [state] = useState<StateProps>({
+    isLoading: false,
+    errorMessage: ''
+  })
+
   return (
     <div className={Styles.login}>
       <LoginHeader />
-      <form className={Styles.form} autoComplete="off">
-        <h2>login</h2>
-        <Input type="email" name="email" placeholder="Digite seu E-mail" />
-        <Input type="password" name="password" placeholder="Digite sua senha" />
-        <button className={Styles.submit} type="submit">Entrar</button>
-        <span className={Styles.link}>Criar Conta</span>
-        <FormStatus/>
-      </form>
+      <FormContext.Provider value={state}>
+        <form className={Styles.form} autoComplete="off">
+          <h2>login</h2>
+          <Input type="email" name="email" placeholder="Digite seu E-mail" />
+          <Input type="password" name="password" placeholder="Digite sua senha" />
+          <button className={Styles.submit} type="submit">Entrar</button>
+          <span className={Styles.link}>Criar Conta</span>
+          <FormStatus/>
+        </form>
+      </FormContext.Provider>
       <Footer />
     </div>
   )
