8ee244e94e4a7cea2f97a7791489ccf0c9ea1e31
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-07-28 20:34:33 -0300

    test: prevent Login to submit authentication more than once at time

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -136,7 +136,7 @@ describe('Login Component', () => {
     expect(spinner).toBeTruthy()
   })
 
-  test('Should call authentication with correct values', () => {
+  test('Should call Authentication with correct values', () => {
     const { sut, authenticationSpy } = makeSut()
     const email = faker.internet.email()
     const password = faker.internet.password()
@@ -144,5 +144,13 @@ describe('Login Component', () => {
     simulateValidSubmit(sut, email, password)
 
     expect(authenticationSpy.params).toEqual({ email, password })
+  })
+
+  test('Should call Authentication only once', () => {
+    const { sut, authenticationSpy } = makeSut()
+    simulateValidSubmit(sut)
+    simulateValidSubmit(sut)
+
+    expect(authenticationSpy.callsCount).toBe(1)
   })
 })
