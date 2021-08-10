4635211f7b7e8a1002b0740767f8c51fa7f581aa
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-08-10 17:49:51 -0300

    refactor: move SUT creation to a facktory method

--- src/presentation/validation/required-field/required-field-validation.spec.ts
+++ src/presentation/validation/required-field/required-field-validation.spec.ts
@@ -2,16 +2,18 @@ import { RequiredFieldError } from '@/presentation/validation/errors'
 import faker from 'faker'
 import { RequiredFieldValidation } from './required-field-validation'
 
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.database.column())
+
 describe('RequiredFieldValidation', () => {
   test('Should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
   })
 
   test('Should return falsy if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.random.word())
 
     expect(error).toBeFalsy()
