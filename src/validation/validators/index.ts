7b21bd98b1498a813d82da6deaadb44f9c3169e8
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-08-22 23:20:43 -0300

    feat: create a index filed to export files more easily

---
+++ src/validation/validators/index.ts
@@ -0,0 +1,4 @@
+export * from './email/email-validation'
+export * from './min-lenght/min-length-validation'
+export * from './required-field/required-field-validation'
+export * from './validation-composite/validation-composite'
