47837665495ea0ef8e7d526e7ae023d5cbb3cdcf
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-08-28 00:53:54 -0300

    feat: add Login composition

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components'
 
 import '@/presentation/styles/global.scss'
+import { makeLogin } from './factories/pages/login/login-factory'
 
 ReactDOM.render(
-  <Router/>,
+  <Router makeLogin={makeLogin}/>,
   document.getElementById('main')
 )
