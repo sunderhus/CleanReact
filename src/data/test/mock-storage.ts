4b45bbf1485ff2ae2a245c20f74a6ad990e17624
Sunderhus <matheus.sunderhus@gmail.com>
2021-10-17 15:43:11 -0300

    refactor: add mockstorage import to index file

--- src/data/test/mock-storage.ts
+++ src/data/test/mock-storage.ts
@@ -1,6 +1,6 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
 
-export class SetStorageSpy<T> implements SetStorage<T> {
+export class SetStorageMock<T> implements SetStorage<T> {
   key: string;
   value: T;
 
