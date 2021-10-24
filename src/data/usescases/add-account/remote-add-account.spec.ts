b786c0d80bd8e71f2b9f9fb62d5c96c4886af6a2
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-10-24 00:09:29 -0300

    test: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 404

--- src/data/usescases/add-account/remote-add-account.spec.ts
+++ src/data/usescases/add-account/remote-add-account.spec.ts
@@ -57,6 +57,15 @@ describe('RemoteAddAccount', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
+  test('Should throw UnexpectedError if HttpPostClient return 404', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.notFound
+    }
+    const promise = sut.add(mockAddAccountParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
+
   test('Should throw UnexpectedError if HttpPostClient return 500', async () => {
     const { sut, httpPostClientSpy } = makeSut()
     httpPostClientSpy.response = {
