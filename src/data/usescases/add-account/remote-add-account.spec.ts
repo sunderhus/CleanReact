ab896ed6df6b19cde9d600197a30a876c73a371a
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-10-23 23:36:22 -0300

    test: ensure RemoteAddAccount calls httpPost client with correct body

--- src/data/usescases/add-account/remote-add-account.spec.ts
+++ src/data/usescases/add-account/remote-add-account.spec.ts
@@ -29,4 +29,12 @@ describe('RemoteAddAccount', () => {
 
     expect(httpPostClientSpy.url).toBe(url)
   })
+
+  test('Should call HttpPostClient with correct Body', async () => {
+    const { sut, httpPostClientSpy } = makeSut()
+    const addAccountParams = mockAddAccountParams()
+    await sut.add(addAccountParams)
+
+    expect(httpPostClientSpy.body).toEqual(addAccountParams)
+  })
 })
