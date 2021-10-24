fd1c2c88604afa3976fef4792981b94cf0e52f81
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-10-24 13:56:10 -0300

    test: ensure CompareFieldsValidation returns falsy when validation succeeds

--- src/validation/validators/compare-fields/compare-field-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-field-validation.spec.ts
@@ -3,19 +3,27 @@ import { FieldValidation } from '@/validation/protocolos/field-validation'
 import faker from 'faker'
 
 class CompareFieldsValidation implements FieldValidation {
-  constructor (readonly field: string) {}
+  constructor (readonly field: string, private readonly valueToCompare: string) {}
 
   validate (value: string): InvalidFieldError {
-    return new InvalidFieldError()
+    return value === this.valueToCompare ? null : new InvalidFieldError()
   }
 }
 
-const makeSut = (): CompareFieldsValidation => new CompareFieldsValidation(faker.database.column())
+const makeSut = (valueToCompare: string = faker.random.words()): CompareFieldsValidation => new CompareFieldsValidation(faker.database.column(), valueToCompare)
 
 describe('CompareFieldsValidation', () => {
   test('Should return an error if validation fails', () => {
     const sut = makeSut()
     const error = sut.validate('')
     expect(error).toEqual(new InvalidFieldError())
+  })
+
+  test('Should return falsy if validation succeeds ', () => {
+    const value = faker.random.word()
+    const sut = makeSut(value)
+    const error = sut.validate(value)
+
+    expect(error).toBeFalsy()
   })
 })
