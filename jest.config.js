d21c02a97c9b753d210d3b3569c0164c3f539795
Sunderhus <matheus.sunderhus@gmail.com>
2021-11-27 17:12:41 -0300

    chore: configure jest converage to ignore import index files

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,7 @@ module.exports = {
     '<rootDir>/src/**/*.{ts,tsx}',
     '!<rootDir>/src/main/**/*',
     '!<rootDir>/src/presentation/components/router/**/*',
+    '!<rootDir>/src/presentation/pages/index.tsx',
     '!<rootDir>/src/**/index.ts',
     '!**/*d.ts'
   ],
