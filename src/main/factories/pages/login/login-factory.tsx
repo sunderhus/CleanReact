bed641973d29ae8bcce521fc6ef73a4254aab1a6
Sunderhus <matheus.sunderhus@gmail.com>
2021-11-27 17:16:31 -0300

    refactor: fix miss spell on RemoteAuthenticationFactory filename

--- src/main/factories/pages/login/login-factory.tsx
+++ src/main/factories/pages/login/login-factory.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Login } from '@/presentation/pages'
-import { makeRemoteAuthentication } from '@/main/usecases/authentication/remove-authentication-factory'
+import { makeRemoteAuthentication } from '@/main/usecases/authentication/remote-authentication-factory'
 import { makeLocalSaveAccessToken } from '@/main/usecases/save-access-token/local-save-access-token-factory'
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
 
