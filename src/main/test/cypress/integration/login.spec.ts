4334c43653ed3edbb73b7f20ec0c134785908577
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-12-08 09:45:31 -0300

    test: ensure Login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -3,7 +3,14 @@ describe('Login', () => {
   beforeEach(() => {
     cy.visit('/login')
   })
-  it('Should load with correct initial state', () => {
-    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo obrigatório')
+  it('Should begin with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
