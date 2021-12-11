2af96e641251ba7b62922ff8c0ea72a775f68fae
Sunderhus <matheus.sunderhus@gmail.com>
2021-12-11 19:15:09 -0300

    feat: ensure SaveAccessToken throws when invalid accessToken are provided

--- src/data/usescases/save-access-token/local-save-access-token.ts
+++ src/data/usescases/save-access-token/local-save-access-token.ts
@@ -1,10 +1,14 @@
 import { SetStorage } from '@/data/protocols/cache/set-storage'
+import { InvalidCredentialsError } from '@/domain/Errors'
 import { SaveAccessToken } from '@/domain/usecases/save-access-token'
 
 export class LocalSaveAcessToken implements SaveAccessToken {
   constructor (private readonly setStorage: SetStorage<string>) {}
 
   async save (accessToken: string): Promise<void> {
+    if (!accessToken) {
+      throw new InvalidCredentialsError()
+    }
     await this.setStorage.set('accessToken', accessToken)
   }
 }
