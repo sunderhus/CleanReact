fc3f81b500823b47b51b7fd538fd85d483956a2e
Sunderhus <matheus.sunderhus@gmail.com>
2021-12-19 17:28:28 -0300

    test: ensure Signup form initiate with correct state

---
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -0,0 +1,17 @@
+import * as FormHelper from '../support/form-helper'
+
+describe('Signup', () => {
+  beforeEach(() => {
+    cy.visit('/signup')
+  })
+
+  it('Should begin with initial state', () => {
+    FormHelper.testInputStatus('name', 'Campo obrigatório')
+    FormHelper.testInputStatus('email', 'Campo obrigatório')
+    FormHelper.testInputStatus('password', 'Campo obrigatório')
+    FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+})
