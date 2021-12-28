9633b426091211c8ab744a9b976a411da325ff8f
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2021-12-28 10:52:32 -0300

    refactor: move sut  to a factory method

--- src/data/usescases/remote-load-survey-list.ts/remote-load-survey-list.spec.ts
+++ src/data/usescases/remote-load-survey-list.ts/remote-load-survey-list.spec.ts
@@ -3,12 +3,31 @@ import { HttpGetClientMock } from '@/data/test'
 import { SurveyModel } from '@/domain/models'
 import faker from 'faker'
 
+type SutParams = {
+  url: string
+}
+
+type SutTypes = {
+  sut: RemoteLoadSurveyList
+  httpGetClientMock: HttpGetClientMock<SurveyModel[]>
+}
+
+const makeSut = ({ url }: SutParams): SutTypes => {
+  const httpGetClientMock = new HttpGetClientMock<SurveyModel[]>()
+  const sut = new RemoteLoadSurveyList(url, httpGetClientMock)
+  return {
+    sut,
+    httpGetClientMock
+  }
+}
+
 describe('RemoteLoadSurveyList', () => {
   it('Should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url()
-    const httpGetClientMock = new HttpGetClientMock<SurveyModel[]>()
-    const sut = new RemoteLoadSurveyList(url, httpGetClientMock)
+    const { sut, httpGetClientMock } = makeSut({ url })
+
     await sut.loadAll()
+
     expect(httpGetClientMock.url).toBe(url)
   })
 })
