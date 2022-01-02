287b560fba3b3db8ac7017ac7eba37a29717df9b
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-01-02 12:06:12 -0300

    test: ensure RemoteLoadSurveyList throws on unexpected error on 403

--- src/data/usescases/remote-load-survey-list.ts/remote-load-survey-list.spec.ts
+++ src/data/usescases/remote-load-survey-list.ts/remote-load-survey-list.spec.ts
@@ -1,33 +1,41 @@
+import { UnexpectedError } from '@/domain/Errors/unexpected-error'
 import { RemoteLoadSurveyList } from './remote-load-survey-list'
-import { HttpGetClientMock } from '@/data/test'
+import { HttpGetClientSpy } from '@/data/test'
 import { SurveyModel } from '@/domain/models'
 import faker from 'faker'
-
-type SutParams = {
-  url: string
-}
+import { HttpStatusCode } from '@/data/protocols/http'
 
 type SutTypes = {
   sut: RemoteLoadSurveyList
-  httpGetClientMock: HttpGetClientMock<SurveyModel[]>
+  httpGetClientSpy: HttpGetClientSpy<SurveyModel[]>
 }
 
-const makeSut = ({ url = faker.internet.url() }: SutParams): SutTypes => {
-  const httpGetClientMock = new HttpGetClientMock<SurveyModel[]>()
-  const sut = new RemoteLoadSurveyList(url, httpGetClientMock)
+const makeSut = (url = faker.internet.url()): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy<SurveyModel[]>()
+  const sut = new RemoteLoadSurveyList(url, httpGetClientSpy)
   return {
     sut,
-    httpGetClientMock
+    httpGetClientSpy
   }
 }
 
 describe('RemoteLoadSurveyList', () => {
   it('Should call HttpGetClient with correct URL', async () => {
     const url = faker.internet.url()
-    const { sut, httpGetClientMock } = makeSut({ url })
+    const { sut, httpGetClientSpy } = makeSut(url)
 
     await sut.loadAll()
 
-    expect(httpGetClientMock.url).toBe(url)
+    expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  it('Should return unexpected error when HttpGetClient returns 403', async () => {
+    const { sut, httpGetClientSpy } = makeSut()
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden
+    }
+    const promise = sut.loadAll()
+
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
