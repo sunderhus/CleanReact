ee35eeef2df5177d63c0e730f5df913592ca602f
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-01-02 21:22:43 -0300

    refactor: change imports to be relative

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -1,5 +1,5 @@
-import { HttpGetParams } from './../../../data/protocols/http/http-get-client'
-import { HttpPostParams, HttpResponse, IHttpPostClient } from '@/data/protocols/http'
+
+import { HttpPostParams, HttpGetParams, HttpResponse, IHttpPostClient } from '@/data/protocols/http'
 import axios, { AxiosResponse } from 'axios'
 
 export class AxiosHttpClient<BodyType = unknown, ResponseType= unknown> implements IHttpPostClient<BodyType, ResponseType> {
