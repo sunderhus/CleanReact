1295033ee316c18567fea853dd33d478e68f8f41
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-01-06 12:00:04 -0300

    refactor: add type safe to makeAxiosHttpClient method

--- src/main/factories/http/axios-http-client-factory.ts
+++ src/main/factories/http/axios-http-client-factory.ts
@@ -1,4 +1,4 @@
 import { AxiosHttpClient } from '@/infra/http/axios-http-client/axios-http-client'
-export const makeAxiosHttpClient = (): AxiosHttpClient => {
-  return new AxiosHttpClient()
+export const makeAxiosHttpClient = <BodyType=unknown, ResponseType=unknown>(): AxiosHttpClient<BodyType, ResponseType> => {
+  return new AxiosHttpClient<BodyType, ResponseType>()
 }
