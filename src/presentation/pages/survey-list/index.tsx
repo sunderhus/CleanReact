ad4ba6dbd326f24d658ce0c4e9684c483d6083e2
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-01-09 16:35:51 -0300

    feat: add survey icon

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -19,39 +19,10 @@ export const SurveyList: React.FC = () => {
         <ul>
           <li>
             <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>01</span>
-                <span className={Styles.month}>05</span>
-                <span className={Styles.year}>2022</span>
-              </time>
-              <p>Qual é o seu framwork favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>01</span>
-                <span className={Styles.month}>05</span>
-                <span className={Styles.year}>2022</span>
-              </time>
-              <p>Qual é o seu framwork favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
-              <time>
-                <span className={Styles.day}>01</span>
-                <span className={Styles.month}>05</span>
-                <span className={Styles.year}>2022</span>
-              </time>
-              <p>Qual é o seu framwork favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li>
-            <div className={Styles.surveyContent}>
+              <div className={[Styles.iconWrap, Styles.pending].join(' ')}>
+                <img className={Styles.icon} src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAASCAYAAABb0P4QAAAAAXNSR0IArs4c6QAAAERlWElmTU0AKgAAAAgAAYdpAAQAAAABAAAAGgAAAAAAA6ABAAMAAAABAAEAAKACAAQAAAABAAAAFKADAAQAAAABAAAAEgAAAAA9nQVdAAAA70lEQVQ4Ea2RPQoCQQyFZ/w5g72lYOEVPIiV2IkIHmCvIZ5D77BgZWtrYWe1ICiuL8tEwjIZZmYNZCf7knyTzRrjrK7rAfwAr+AheyNZwiei98gNrBkISxYjz5KbZb0V4gXxlN8jzo+1tk91BOT6nhPmOFNg1Nb0UiCNxY0Uu8QW044BuMIZHs3DJzcra3/yOgem3UoT3pEcaQUh3TchAX9/KNTsy/mAtLebrzhXI+AqE/oQl55ErIfYxp5WothW71QyAJ0VWKG06DJAQ/jTA0yH0TUAzf4Gc8BFC5g3GcHI3IQvBy0asesDsB08CfYFB/44kX6+Hj8AAAAASUVORK5CYII=" alt="thumbs up" />
+
+              </div>
               <time>
                 <span className={Styles.day}>01</span>
                 <span className={Styles.month}>05</span>
