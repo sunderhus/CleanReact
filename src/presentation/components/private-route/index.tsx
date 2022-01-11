42e2fd1229456a3b90b9b5df3fbb763301d9e703
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-01-11 17:08:58 -0300

    feat: ensure PrivateRoute renders correct component when token is not empty

--- src/presentation/components/private-route/index.tsx
+++ src/presentation/components/private-route/index.tsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Redirect, Route, RouteProps } from 'react-router-dom'
+import { ApiContext } from '@/presentation/contexts'
 
 const PrivateRoute: React.FC<RouteProps> = (props: RouteProps) => {
-  return <Route {...props} component={() => <Redirect to="/login" />}/>
+  const { getCurrentAccount } = useContext(ApiContext)
+
+  return getCurrentAccount()?.accessToken
+    ? <Route {...props} />
+    : <Route {...props} component={() => <Redirect to="/login" />}/>
 }
 
 export default PrivateRoute
