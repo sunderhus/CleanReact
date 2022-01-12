1b418578869a27c319ea9987373b84b91d87bb6b
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-01-12 12:21:33 -0300

    refactor: ensure to use query instead of get on test finders

--- src/presentation/test/form-helper.tsx
+++ src/presentation/test/form-helper.tsx
@@ -2,9 +2,9 @@ import { act, fireEvent, screen } from '@testing-library/react'
 import faker from 'faker'
 
 export const testStatusForField = (fieldName: string, validationError: string = ''): void => {
-  const fieldWrap = screen.getByTestId(`${fieldName}-wrap`)
-  const field = screen.getByTestId(`${fieldName}`)
-  const label = screen.getByTestId(`${fieldName}-label`)
+  const fieldWrap = screen.queryByTestId(`${fieldName}-wrap`)
+  const field = screen.queryByTestId(`${fieldName}`)
+  const label = screen.queryByTestId(`${fieldName}-label`)
 
   expect(
     fieldWrap.getAttribute('data-invalid')
@@ -14,17 +14,17 @@ export const testStatusForField = (fieldName: string, validationError: string = 
 }
 
 export const populateField = (fieldName: string, value = faker.random.word()): void => {
-  const input = screen.getByTestId(fieldName)
+  const input = screen.queryByTestId(fieldName)
   act(() => {
     fireEvent.input(input, { target: { value: value } })
   })
 }
 
 export const testElementExists = (testId: string): void => {
-  expect(screen.getByTestId(testId)).toBeTruthy()
+  expect(screen.queryByTestId(testId)).toBeTruthy()
 }
 
 export const testElementText = (testId: string, text: string): void => {
-  const element = screen.getByTestId(testId)
+  const element = screen.queryByTestId(testId)
   expect(element.textContent).toBe(text)
 }
