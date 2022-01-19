b92ab4c1a2813ee88020de6a35aa903604e5ff4d
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-01-19 13:59:01 -0300

    test: ensure SurveyList component calls LoadSurveylist

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import { screen, render } from '@testing-library/react'
 import SurveyList from '.'
+import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
+import { SurveyModel } from '@/domain/models'
 
-const makeSut = (): void => {
-  render(<SurveyList/>)
+class LoadSurveyListSpy implements LoadSurveyList {
+  callsCount= 0;
+  async loadAll (): Promise<SurveyModel[]> {
+    this.callsCount++
+
+    return await Promise.resolve(null)
+  }
+}
+interface SutTypes {
+  loadSurveyList: LoadSurveyListSpy
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy}/>)
+
+  return {
+    loadSurveyList: loadSurveyListSpy
+  }
 }
 
 describe('SurveyList', () => {
@@ -11,5 +30,11 @@ describe('SurveyList', () => {
     makeSut()
     const emptyList = screen.queryByRole('list')
     expect(emptyList.children.length).toBe(4)
+  })
+
+  it('Should call  LoadSurveyList on start', () => {
+    const { loadSurveyList } = makeSut()
+
+    expect(loadSurveyList.callsCount).toBe(1)
   })
 })
