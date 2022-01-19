145f4b9c077f49eebdaec93cb8c18fc1dde2848f
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-01-19 13:28:32 -0300

    feat: ensure SurveyList present 4 empty list items on start

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -1,5 +1,6 @@
+import { Footer, Header } from '@/presentation/components'
 import React from 'react'
-import { Header, Footer, Icon, IconName } from '@/presentation/components'
+import { SurveyItemEmpty } from './components/'
 import Styles from './styles.scss'
 
 export const SurveyList: React.FC = () => {
@@ -9,19 +10,7 @@ export const SurveyList: React.FC = () => {
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
         <ul>
-          <li>
-            <div className={Styles.surveyContent}>
-              <Icon className={Styles.surveyIcon} iconName={IconName.thumbUp} />
-              <time>
-                <span className={Styles.day}>01</span>
-                <span className={Styles.month}>05</span>
-                <span className={Styles.year}>2022</span>
-              </time>
-              <p>Qual é o seu framwork favorito?</p>
-            </div>
-            <footer>Ver resultado</footer>
-          </li>
-          <li></li>
+          <SurveyItemEmpty />
         </ul>
       </div>
       <Footer />
