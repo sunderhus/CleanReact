155e200f779fd4a2ec4a1f179610a42944ffd3eb
Matheus Sunderhus <matheus.sunderhus@gmail.com>
2022-02-08 20:37:17 -0300

    feat: ensure survey-list present error on LoadSurveylist fails

--- src/presentation/pages/survey-list/index.tsx
+++ src/presentation/pages/survey-list/index.tsx
@@ -11,13 +11,17 @@ interface Props {
 
 export const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
   const [state, setState] = useState({
-    surveys: [] as SurveyModel[]
+    surveys: [] as SurveyModel[],
+    error: ''
   })
 
   useEffect(() => {
     loadSurveyList.loadAll()
       .then((surveys) => {
-        setState({ surveys })
+        setState(current => ({ ...current, surveys }))
+      })
+      .catch((error) => {
+        setState(current => ({ ...current, error: error.message }))
       })
   }, [])
 
@@ -26,16 +30,20 @@ export const SurveyList: React.FC<Props> = ({ loadSurveyList }: Props) => {
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Enquetes</h2>
-        <ul data-testid="survey-list">
-          {state.surveys.length
-            ? state.surveys.map(survey => {
-              return (
-                <SurveyItem key={survey.id} survey={survey} />
-              )
-            })
-            : < SurveyItemEmpty />}
+        {!state.error ? (
+          <ul data-testid="survey-list">
+            {state.surveys.length
+              ? state.surveys.map(survey => {
+                return (
+                  <SurveyItem key={survey.id} survey={survey} />
+                )
+              })
+              : < SurveyItemEmpty />}
+          </ul>
+        ) : (
+          <div data-testid="error">{state.error}</div>
+        )}
 
-        </ul>
       </div>
       <Footer />
     </div>
