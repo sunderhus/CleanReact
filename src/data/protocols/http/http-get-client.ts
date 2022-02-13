646daf80f678b6664c9c76c2289017bb8eb1451b
Sunderhus <matheus.sunderhus@gmail.com>
2022-02-13 14:35:40 -0300

    feat: ensure AxiosHttpClientAdapter calls axios with correct values

--- src/data/protocols/http/http-get-client.ts
+++ src/data/protocols/http/http-get-client.ts
@@ -2,6 +2,7 @@ import { HttpResponse } from './http-response'
 
 export type HttpGetParams = {
   url: string
+  headers?: unknown
 }
 
 export interface IHttpGetClient<R=unknown>{
--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -18,7 +18,7 @@ export class AxiosHttpClient<BodyType = unknown, ResponseType= unknown> implemen
     let axiosResponse: AxiosResponse<ResponseType>
 
     try {
-      axiosResponse = await axios.get(params.url)
+      axiosResponse = await axios.get(params.url, { headers: params.headers })
     } catch (error) {
       axiosResponse = error.response
     }
