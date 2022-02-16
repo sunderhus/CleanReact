0d53d5941aaf1e34e42df745982dbbe2597c03fd
Sunderhus <matheus.sunderhus@gmail.com>
2022-02-15 22:47:46 -0300

    refactor: improve imports on remote-load-survey-list-factory

--- src/main/usecases/load-survey-list/remote-load-survey-list-factory.ts
+++ src/main/usecases/load-survey-list/remote-load-survey-list-factory.ts
@@ -1,5 +1,5 @@
-import { RemoteLoadSurveyList } from '@/data/usescases/load-survey-list.ts/remote-load-survey-list'
-import { LoadSurveyList } from '@/domain/usecases/load-survey-list'
+import { RemoteLoadSurveyList } from '@/data/usescases'
+import { LoadSurveyList } from '@/domain/usecases'
 import { makeAuthorizeHttpGetClientDecorator } from '@/main/factories/decorators/authorize-http-get-client-decorator-factory'
 import { makeApiUrl } from '@/main/factories/http/api-url-factory'
 
