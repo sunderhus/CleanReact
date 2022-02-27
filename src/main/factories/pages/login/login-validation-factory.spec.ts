6200a1b3f050411b61efe5d6b0ec7fcc8f9fafab
Sunderhus <matheus.sunderhus@gmail.com>
2022-02-27 17:58:29 -0300

    refactor: ensure LoginValidationFactory to have correct validations

--- src/main/factories/pages/login/login-validation-factory.spec.ts
+++ src/main/factories/pages/login/login-validation-factory.spec.ts
@@ -1,14 +1,15 @@
 import { makeLoginValidation } from '@/main/factories/pages/login/login-validation-factory'
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+import { EmailValidation, MinLengthValidation, RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
 
 describe('LoginValidationFactory', () => {
   it('Should make ValidationComposite with correct validations', () => {
     const composite = makeLoginValidation()
     expect(composite).toEqual(
       ValidationComposite.build([
-        ...Builder.field('email').required().email().build(),
-        ...Builder.field('password').required().min(5).build()
+        new RequiredFieldValidation('email'),
+        new EmailValidation('email'),
+        new RequiredFieldValidation('password'),
+        new MinLengthValidation('password', 5)
       ])
     )
   })
