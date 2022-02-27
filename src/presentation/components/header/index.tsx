648cc7e0f6e9a14515303668b078327a8195a1ce
Sunderhus <matheus.sunderhus@gmail.com>
2022-02-27 18:32:16 -0300

    refactor: add useLogout on header

--- src/presentation/components/header/index.tsx
+++ src/presentation/components/header/index.tsx
@@ -1,17 +1,16 @@
 import Logo from '@/presentation/components/logo'
 import { ApiContext } from '@/presentation/contexts'
+import { useLogout } from '@/presentation/hooks'
 import React, { useContext } from 'react'
-import { useHistory } from 'react-router-dom'
 import Styles from './styles.scss'
 
 const Header: React.FC = () => {
-  const { setCurrentAccount, getCurrentAccount } = useContext(ApiContext)
-  const history = useHistory()
+  const { getCurrentAccount } = useContext(ApiContext)
+  const logout = useLogout()
 
   const handleLogout = (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
     event.preventDefault()
-    setCurrentAccount(undefined)
-    history.replace('/login')
+    logout()
   }
 
   return (
