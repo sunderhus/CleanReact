5d6ac88c778729e27ff7f6899d40ab5654e44908
Sunderhus <matheus.sunderhus@gmail.com>
2022-02-27 17:54:26 -0300

    refactor: ensure SignUpValidationFactory to have correct validations

--- src/main/factories/pages/signup/signup-validation-factory.spec.ts
+++ src/main/factories/pages/signup/signup-validation-factory.spec.ts
@@ -1,5 +1,5 @@
-import { ValidationComposite } from '@/validation/validators'
-import { ValidationBuilder as Builder } from '@/validation/validators/builder/validation-builder'
+import { EmailValidation, MinLengthValidation, RequiredFieldValidation, ValidationComposite } from '@/validation/validators'
+import { CompareFieldsValidation } from '@/validation/validators/compare-fields/compare-fields.validation'
 import { makeSignUpValidation } from './signup-validation-factory'
 
 describe('SignUpValidationFactory', () => {
@@ -7,10 +7,14 @@ describe('SignUpValidationFactory', () => {
     const composite = makeSignUpValidation()
     expect(composite).toEqual(
       ValidationComposite.build([
-        ...Builder.field('name').required().min(5).build(),
-        ...Builder.field('email').required().email().build(),
-        ...Builder.field('password').required().min(5).build(),
-        ...Builder.field('passwordConfirmation').required().sameAs('password').build()
+        new RequiredFieldValidation('name'),
+        new MinLengthValidation('name', 5),
+        new RequiredFieldValidation('email'),
+        new EmailValidation('email'),
+        new RequiredFieldValidation('password'),
+        new MinLengthValidation('password', 5),
+        new RequiredFieldValidation('passwordConfirmation'),
+        new CompareFieldsValidation('passwordConfirmation', 'password')
       ])
     )
   })
