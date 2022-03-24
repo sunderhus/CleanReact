ce63c5d0dda9865f1304682750ae82d584ebf351
Sunderhus <matheus.sunderhus@gmail.com>
2022-03-24 00:03:23 -0300

    test: ensure RemoteLoadSurveyResult returns access denied error when HttpGetClient returns 403

--- src/data/usescases/load-survey-result/remote-load-survey-result.spec.ts
+++ src/data/usescases/load-survey-result/remote-load-survey-result.spec.ts
@@ -1,4 +1,6 @@
+import { HttpStatusCode } from '@/data/protocols/http'
 import { HttpGetClientSpy } from '@/data/test'
+import { AccessDeniedError } from '@/domain/Errors'
 import faker from 'faker'
 import { RemoteLoadSurveyResult } from './remote-load-survey-result'
 
@@ -17,11 +19,21 @@ const makeSut = (url = faker.internet.url()): SutTypes => {
 }
 
 describe('RemoteLoadSurveyResult', () => {
-  test('Should call HttpGetClient with correct url', async () => {
+  it('Should call HttpGetClient with correct url', async () => {
     const url = faker.internet.url()
     const { sut, httpClientSpy } = makeSut(url)
+
     await sut.load()
 
     expect(httpClientSpy.url).toBe(url)
+  })
+
+  it('Should return access denied error when HttpGetClient returns 403', () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response.statusCode = HttpStatusCode.forbidden
+
+    const promise = sut.load()
+
+    expect(promise).rejects.toThrow(new AccessDeniedError())
   })
 })
