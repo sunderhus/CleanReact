c02ba3ee798a92e0dd1b6613a16a39ce667f0640
Sunderhus <matheus.sunderhus@gmail.com>
2022-03-26 20:12:40 -0300

    feat: add skeleton with shimmer on SurveyResult

--- src/presentation/pages/survey-result/index.tsx
+++ src/presentation/pages/survey-result/index.tsx
@@ -9,30 +9,34 @@ const SurveyResult: React.FC = () => {
     <div className={Styles.surveyResultWrap}>
       <Header />
       <div className={Styles.contentWrap}>
-        <hgroup>
-          <Calendar date={new Date()} className={Styles.calendarWrap} />
-          <h2>{faker.random.words(15)}</h2>
-        </hgroup>
+        {false && (
+          <>
+            <hgroup>
+              <Calendar date={new Date()} className={Styles.calendarWrap} />
+              <h2>{faker.random.words(15)}</h2>
+            </hgroup>
 
-        <FlipMove className={Styles.answersList}>
-          <li>
-            <img src={faker.image.imageUrl()} alt="fake photo" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li className={Styles.active}>
-            <img src={faker.image.imageUrl()} alt="fake photo" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-          <li>
-            <img src={faker.image.imageUrl()} alt="fake photo" />
-            <span className={Styles.answer}>ReactJS</span>
-            <span className={Styles.percent}>50%</span>
-          </li>
-        </FlipMove>
+            <FlipMove className={Styles.answersList}>
+              <li>
+                <img src={faker.image.imageUrl()} alt="fake photo" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li className={Styles.active}>
+                <img src={faker.image.imageUrl()} alt="fake photo" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+              <li>
+                <img src={faker.image.imageUrl()} alt="fake photo" />
+                <span className={Styles.answer}>ReactJS</span>
+                <span className={Styles.percent}>50%</span>
+              </li>
+            </FlipMove>
 
-        <button>Voltar</button>
+            <button>Voltar</button>
+          </>
+        )}
 
         {false && <Loading />}
       </div>
