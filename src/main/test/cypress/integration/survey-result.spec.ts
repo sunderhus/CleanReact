405cac26a60a9583a1312ac1f6ed01e3ab6fe2cf
Sunderhus <matheus.sunderhus@gmail.com>
2022-04-11 23:32:14 -0300

    test: ensure SurveyResult present correct data on success

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -31,6 +31,7 @@ describe('SurveyResult', () => {
     mockUnexpectedError()
     cy.getByTestId('error').should('exist')
     mockSuccess()
+
     cy.getByTestId('reload').click()
 
     cy.getByTestId('answers').should('exist')
@@ -40,5 +41,26 @@ describe('SurveyResult', () => {
     mockAccessDeniedError()
 
     Helper.testUrl('/login')
+  })
+
+  it('Should present surveyResult', () => {
+    mockSuccess()
+
+    cy.getByTestId('answers').children('li:nth-child(1)').as('first-surveyResult')
+    cy.getByTestId('answers').children('li:nth-child(1)').children('img').as('first-surveyResult-image')
+    cy.getByTestId('answers').children('li:nth-child(2)').as('second-surveyResult')
+
+    cy.getByTestId('question').should('contain.text', 'Question 1')
+    cy.getByTestId('day').should('contain.text', '11')
+    cy.getByTestId('month').should('contain.text', 'abr')
+    cy.getByTestId('year').should('contain.text', '2022')
+    cy.get('@first-surveyResult')
+      .should('contain.text', 'any_answer')
+      .should('contain.text', '12%')
+    cy.get('@first-surveyResult-image')
+      .should('have.attr', 'src', 'any_image')
+    cy.get('@second-surveyResult')
+      .should('contain.text', 'any_answer_2')
+      .should('contain.text', '88%')
   })
 })
